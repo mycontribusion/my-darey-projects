49a5d8c430fdb90553c6aa98dd9226299e28a09c
mycontribusion <kali@kali.kali>
2025-05-01 13:09:59 +0100

    inputField replaces doc.get...

--- task-three-calc.js
+++ task-three-calc.js
@@ -1,41 +1,42 @@
 
+const inputField = document.getElementById("input-feild");
 const btn = document.querySelectorAll('.buttons button');
-    btn.forEach((button) => {
-        button.addEventListener('click', () => {
-            let value = button.value || button.innerText;
-            let operators = ["+","-","*","/"];
-            
-            if (value == 'C') {
-                document.getElementById("input-feild").value = '';
-            } else if (operators.includes(value)) {
-                let content = document.getElementById("input-feild").value;
-                lengthofcontent = content.length;
-                let laststring = content[lengthofcontent-1];
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        let value = button.value || button.innerText;
+        let operators = ["+","-","*","/"];
+        
+        if (value === 'C') {
+            inputField.value = '';
+        } else if (operators.includes(value)) {
+            let content = inputField.value;
+            let lengthofcontent = content.length;
+            let laststring = content[lengthofcontent-1];
 
-                if (document.getElementById("input-feild").value =='') {
-                    document.getElementById("input-feild").value ==''
-                } else if (operators.includes(laststring)){
-                } else {
-                document.getElementById("input-feild").value += value;
-            }} else if (value == '='){
-                let content = document.getElementById("input-feild").value;
-                lengthofcontent = content.length;
-                let laststring = content[lengthofcontent-1];
-
-                if (operators.includes(laststring)){
-                    content = content.slice(0, content.length - 1);
-                    console.log(content);
-                    document.getElementById("input-feild").value = new Function('return ' + content)();
-                } else {
-                    if (document.getElementById("input-feild").value =='') {
-                    } else {
-                        document.getElementById("input-feild").value = new Function('return ' + document.getElementById("input-feild").value)();
-                    }
-                }
-                
+            if (inputField.value =='') {
+                inputField.value =''
+            } else if (operators.includes(laststring)){
             } else {
-                document.getElementById("input-feild").value += value;
+            inputField.value += value;
+        }} else if (value === '='){
+            let content = inputField.value;
+            let lengthofcontent = content.length;
+            let laststring = content[lengthofcontent-1];
+
+            if (operators.includes(laststring)){
+                content = content.slice(0, content.length - 1);
+                console.log(content);
+                inputField.value = new Function('return ' + content)();
+            } else {
+                if (inputField.value ==='') {
+                } else {
+                    inputField.value = new Function('return ' + document.getElementById("input-feild").value)();
+                }
             }
-        });
+            
+        } else {
+            inputField.value += value;
+        }
     });
+});
 
